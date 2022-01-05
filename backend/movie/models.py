99072875fc27122bf0f228fc1db0c1853bce1373
tylerasa <sarpongasaresylvester@gmail.com>
2022-01-05 16:16:32 +0000

    creating movie and actor models

--- backend/movie/models.py
+++ backend/movie/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    comment = models.TextField()
+    synopsis = models.TextField()
+    rating = models.FloatField(default=0)
+    your_rating = models.FloatField(default=0)
+    reviewed = models.BooleanField(default=False)
+    release_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+class Actor(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
