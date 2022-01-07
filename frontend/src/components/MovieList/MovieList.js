a2b74cab2ebd8577b91c0898515fc031417b399c
tylerasa <sarpongasaresylvester@gmail.com>
2022-01-07 11:35:01 +0000

    vertically aligining rating center

--- frontend/src/components/MovieList/MovieList.js
+++ frontend/src/components/MovieList/MovieList.js
@@ -12,18 +12,73 @@ const MovieList = () => {
       .then((res) => setMoives(res.data.results));
   }, []);
   return (
-    <div className="grid-container">
+    <div
+      style={{
+        width: "100%",
+        maxWidth: "1400px",
+        overflowX: "scroll",
+        overflowY: "hidden",
+        display: "flex",
+        paddingTop: "20px",
+      }}
+      // className="grid-container"
+    >
       {movies
         ? movies.map((ele, i) => {
             return (
-              <div style={{ backgroundColor: "crimson", margin: "auto" }} key={i}>
-                <img
-                  src={`https://image.tmdb.org/t/p/w500/` + ele.poster_path}
-                  width="150px"
-                  height="225px"
-                  style={{borderRadius: "8px"}}
-                />
-                <p style={{fontSize: "14px", fontWeight: "bold"}}>{ele.title}</p>
+              <div
+                style={{
+                  // backgroundColor: "crimson",
+                  // margin: "auto",
+                  width: "150px",
+                  minWidth: "150px",
+                  marginLeft: i === 0 ? "40px" : "20px",
+                  // marginLeft: "20px"
+                }}
+                key={i}
+                className="card_style_1"
+              >
+                <div>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500/` + ele.poster_path}
+                    width="150px"
+                    height="225px"
+                    style={{ borderRadius: "8px" }}
+                  />
+                </div>
+                <div
+                  style={{ padding: "30px 10px 10px", position: "relative" }}
+                >
+                  <div
+                    style={{
+                      backgroundColor: "#222",
+                      width: "38px",
+                      height: "38px",
+                      borderRadius: "50%",
+                      position: "absolute",
+                      top: "-19px",
+                      color: "white",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignContent: "center",
+                      fontSize: "0.7em",
+                    }}
+                  >
+                    <span
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                      }}
+                    >
+                      89
+                    </span>
+                  </div>
+                  <p
+                    style={{ fontSize: "14px", fontWeight: "bold", margin: 0 }}
+                  >
+                    {ele.title}
+                  </p>
+                </div>
               </div>
             );
           })
