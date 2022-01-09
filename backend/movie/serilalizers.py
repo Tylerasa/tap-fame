88dc4f0fe24d6e9cfb43cde67b6bb24ad8ffa8c5
tylerasa <sarpongasaresylvester@gmail.com>
2022-01-09 11:31:21 +0000

    removing actor model

--- backend/movie/serilalizers.py
+++ backend/movie/serilalizers.py
@@ -1,15 +1,8 @@
-from .models import Actor, Movie
+from .models import  Movie
 from rest_framework import serializers
 from .models import User
-class ActorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Actor
-        fields = '__all__'
-
 
 class MovieSerializer(serializers.ModelSerializer):
-    actor = ActorSerializer(source='actor_set', many=True)
-
     class Meta:
         model  = Movie
         fields = '__all__'
