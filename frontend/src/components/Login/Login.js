5f5c314ffc72377612646172a8170cb15d75c8c7
tylerasa <sarpongasaresylvester@gmail.com>
2022-01-09 03:34:28 +0000

    login test succesful in frontend

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,16 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+    var item = {
+      email,
+      password,
+    };
+    axios
+      .post("/auth/login/", item)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div
       style={{
-        displsy: "flex",
+        display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        height: "100vh",
+        width: "100vw",
       }}
     >
-      <div style={{ maxWidth: "300px" }}>
-        <input type="text" />
+      <div
+        style={{
+          maxWidth: "400px",
+          backgroundColor: "#165480",
+          padding: "20px",
+          borderRadius: "10px",
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+          style={{
+            display: "block",
+            padding: "8px",
+            width: "100%",
+            borderRadius: "18px",
+            fontSize: "16px",
+            outline: "#ddd",
+            fontFamily: "Inter",
+            borderWidth: 0,
+            marginBottom: "20px",
+          }}
+          placeholder="Email"
+        />
+
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          style={{
+            display: "block",
+            padding: "8px",
+            width: "100%",
+            borderRadius: "18px",
+            fontSize: "16px",
+            outline: "#ddd",
+            fontFamily: "Inter",
+            borderWidth: 0,
+            // margin: "auto",
+          }}
+          placeholder="Password"
+        />
+        <div
+          onClick={handleSubmit}
+          style={{
+            cursor: "pointer",
+            background: "white",
+            marginTop: "20px",
+            padding: "8px 15px",
+            borderRadius: "18px",
+          }}
+        >
+          Login
+        </div>
       </div>
     </div>
   );
